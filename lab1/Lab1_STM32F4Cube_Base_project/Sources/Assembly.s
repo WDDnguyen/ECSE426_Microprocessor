5737c2943163338ae54a60c64960674326c8a1d4
William Nguyen <WDDnguyen@gmail.com>
2017-02-02 19:56:26 -0500

    Added comment to assembly

--- lab1/Lab1_STM32F4Cube_Base_project/Sources/Assembly.s
+++ lab1/Lab1_STM32F4Cube_Base_project/Sources/Assembly.s
@@ -4,11 +4,11 @@ FIR_asm
 	
 	PUSH{R4-R12}
 	;initialize value
-	SUB R2,R2, #4
-	MOV R5, #0  ;j
+	SUB R2,R2, #4 ; Length - Order
+	MOV R5, #0 ;j
 	
 loop
-	VLDR.f32 S4, =0.0  ;y =0;
+	VLDR.f32 S4, =0.0  ;y =0 ;
 	MOV R4, #0  ;i
 	
 loop2
@@ -17,24 +17,30 @@ loop2
 	
 	;inputArray[k]
 	ADD R7, R0, R6, LSL #2
-	VLDR.f32 S6, [R7] ;x4
+	;load register address to float register s8
+	VLDR.f32 S6, [R7]
 	
 	;coeffArray[i]
 	ADD R8,R3,R4,LSL #2
+	;load register address to float register s8
 	VLDR.f32 S8,[R8]
 	
-	;y += inputArray[k] * coeffArray[i]
+	;temp = inputArray[k] * coeffArray[i]
 	VMUL.f32 S1, S8,S6
+	;y = y + temp
 	VADD.f32 S4, S1,S4
+	;i++
 	ADDS R4,#1
+	;check if i < order + 1
 	CMP R4,#5
 	BNE loop2
 	
 	;OutputArray[j] = y;
 	ADD R7,R1,R5, LSL #2
 	VSTR.f32 S4,[R7] 
-	
+	;j++
 	ADD R5,#1
+	;check if j < length - order
 	CMP R5,R2
 	BLT loop
 	POP{R4-R12}
